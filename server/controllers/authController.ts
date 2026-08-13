7e3f72eb92b5dccf9fbb7d479cc5ef548402f3bc
NikhilxKumarr <nk047647@gmail.com>
2026-08-14 04:44:04 +0530

    Created Api for auth

---
+++ server/controllers/authController.ts
@@ -0,0 +1,137 @@
+import e, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { UserModel } from '../models/User.js';
+import { AuthRequest } from '../middlewares/auth.js';
+
+
+
+const generateToken = (id: string): string => {
+  return jwt.sign(
+    { id },
+    process.env.JWT_SECRET as string,
+    { expiresIn: "30d" }
+  );
+};
+
+
+//Register a new User
+//POST /api/auth/register
+
+export const registerUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { name, email, password, phone, role } = req.body;
+        if (!name || !email || !password) {
+            res.status(400).json({ message: 'Please provide all required fields' });
+            return;
+        }
+
+        // Check if user already exists
+        const existingUser = await UserModel.findOne({ email });
+        if (existingUser) {
+            res.status(400).json({ message: 'User already exists' });
+            return;
+        }
+
+        //Hash password
+        const bcrypt = require('bcryptjs');
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create new user
+        const user = await UserModel.create({
+            name,
+            email,
+            password: hashedPassword,
+            phone,
+            role,
+        });
+
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                role: user.role,
+                token: generateToken(user._id.toString()),
+            });
+        } else {
+            res.status(400).json({ message: 'Invalid user data' });
+        }
+
+       
+       
+   }catch (error : any) {
+           console.error(error);
+      res.status(500).json({ message: 'Server error' });
+    
+
+   }
+};
+
+
+
+
+//Authenticate a User
+//POST /api/auth/login
+
+export const loginUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+         const {  email, password} = req.body;
+        if ( !email || !password) {
+            res.status(400).json({ message: 'Please provide all required fields' });
+            return;
+        }
+
+        // Check for user
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            res.status(400).json({ message: 'Invalid credentials' });
+            return;
+        }
+
+        //Check password
+        const bcrypt = require('bcryptjs');
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            res.status(400).json({ message: 'Invalid credentials' });
+            return;
+        }
+        
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            phone: user.phone,
+            role: user.role,
+            token: generateToken(user._id.toString()),
+        });
+
+
+   }catch (error : any) {
+       console.error(error);
+       res.status(500).json({ message: 'Server error' });
+
+   }
+};
+
+
+
+//Get User Profile
+//POST /api/auth/profile
+//@access Private
+export const getMe = async (req: AuthRequest, res: Response): Promise<void> => {
+    try {
+        if(!req.user){
+            res.status(401).json({ message: 'Not authorized' });
+            return;
+        }
+        res.json(req.user);
+       
+   }catch (error : any) {
+     console.error(error);
+       res.status(500).json({ message: error.message  });
+
+
+   }
+};
--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -16,6 +17,16 @@ const port = process.env.PORT || 5000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
+});
+
+app.use('/api/auth', authRouter);
+//Global error handler
+app.use((err: any, req: Request, res: Response, next: Function) => {
+    console.error("Unhandled Error:", err);
+    res.status(500).json({
+         message: 'Something went wrong!' ,
+        stack: process.env.NODE_ENV === 'production' ? undefined : err.stack,
+    });
 });
 
 app.listen(port, () => {
